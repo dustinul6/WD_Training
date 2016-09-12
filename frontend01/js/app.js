1f62afcccea6105268f8789faea549a243f73f32
Tzu-Yao Lin <dustinul6@gmail.com>
2016-09-12 01:13:16 -0400

    fixed some js styles

--- frontend01/js/app.js
+++ frontend01/js/app.js
@@ -1,46 +1,43 @@
+/*global angular */
+'use strict';
+
 var app = angular.module('myApp', []);
 //  建立一個 angular module, 
 
-
-app.controller('myCtrl', ['$scope', '$http', function($scope, $http){
+app.controller('myCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.name = "Tzu-Yao";
     
     $scope.newProduct = {
-        Name:"",
+        Name: "",
         Description : "",
         Price: 0,
         BrandId : 0
     };
     
     console.log("Hello, ", $scope.name);
+    
     $http.get("http://localhost:56490/odata/Products").then(
         //如果成功，執行
-        function(result){
+        function (result) {
             console.log("result is, ", result);
             $scope.products = result.data.value;
             console.log("products is, ", $scope.products);
         },
         // 如果失敗，執行
-        function(error){
+        function (error) {
             console.log(error);
         }
     );
     
     $http.get("http://localhost:56490/odata/Brands").then(
         //如果成功，執行
-        function(result){
-            
+        function (result) {
             $scope.brands = result.data.value;
-            
         },
         // 如果失敗，執行
-        function(error){
+        function (error) {
             console.log(error);
         }
-        
-    )
+    );
     // $scope.products = 
-    $http.post(route, $scope.newProduct).then(
-        function
-    )
-}])
+}]);
